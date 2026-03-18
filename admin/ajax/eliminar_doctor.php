f9992084f4b6dbf6c9961f6098c22840bbbda72e
Kanji <169074388+Melvinlibera@users.noreply.github.com>
2026-03-18 17:27:26 -0400

    Delete admin directory

--- admin/ajax/eliminar_doctor.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("../../config/db.php");
-$id=$_POST['id'];
-$pdo->prepare("DELETE FROM doctores WHERE id=?")->execute([$id]);
-echo json_encode(['success'=>true,'message'=>"Doctor eliminado"]);
